ae7cbabd48dec55df620774784bde454c07dfa35
Fanta335 <fantayusuke5210@gmail.com>
2021-08-30 17:41:23 +0900

    数学演算機能の追加

--- src/main.ts
+++ src/main.ts
@@ -100,12 +100,165 @@ class CommandList {
   }
 }
 
+class MTools {
+  static commandLineParser(CLIInputString: string) {
+    let parsedStringInputArray = CLIInputString.trim().split(" ");
+    return parsedStringInputArray;
+  }
+
+  static appendResultParagraph(parentDiv: HTMLDivElement, isValid: boolean, message: string) {
+    let promptName = "";
+    let promptColor = "";
+    if (isValid) {
+      promptName = "MTools";
+      promptColor = "turquoise";
+    } else {
+      promptName = "MToolsError";
+      promptColor = "red";
+    }
+    parentDiv.innerHTML += `
+      <p class="m-0">
+        <span style='color: ${promptColor}'>${promptName}</span><span class="text-white">: ${message}</span>
+      </p>
+    `;
+    return;
+  }
+
+  static evaluatedResultsStringFromParsedStringInputArray(parsedStringInputArray: string[]) {
+    let result = 0;
+    let argsArray = parsedStringInputArray[2].split(",").map((stringArgument) => Number(stringArgument));
+    let argA = argsArray[0];
+    let argB = argsArray[1];
+    let commandName = parsedStringInputArray[1];
+
+    if (commandName == "add") result = argA + argB;
+    else if (commandName == "subtract") result = argA - argB;
+    else if (commandName == "multiply") result = argA * argB;
+    else if (commandName == "divide") result = argA / argB;
+    else if (commandName == "exp") result = Math.pow(argA, argB);
+    else if (commandName == "log") result = Math.log(argB) / Math.log(argA);
+    else if (commandName == "sqrt") result = Math.sqrt(argA);
+    else if (commandName == "abs") result = Math.abs(argA);
+    else if (commandName == "round") result = Math.round(argA);
+    else if (commandName == "ceil") result = Math.ceil(argA);
+    else if (commandName == "floor") result = Math.floor(argA);
+    else console.log("MTools.evaluatedResultsStringFromParsedStringInputArray:: invalid command name");
+
+    return "your result is: " + result;
+  }
+
+  static parsedArrayValidator(parsedStringInputArray: string[]) {
+    // すべてのコマンドに適用されるルールに照らし合わせて入力をチェックします。
+    let validatorResponse = MTools.universalValidator(parsedStringInputArray);
+    if (!validatorResponse["isValid"]) return validatorResponse;
+
+    // 入力が最初のvalidatorを通過した場合、どのコマンドが与えられたかに基づいて、より具体的な入力の検証を行います。
+    validatorResponse = MTools.commandArgumentsValidator(parsedStringInputArray.slice(1, 3));
+    if (!validatorResponse["isValid"]) return validatorResponse;
+
+    return { isValid: true, errorMessage: "" };
+  }
+
+  static universalValidator(parsedStringInputArray: string[]) {
+    let validCommandList = ["add", "subtract", "multiply", "divide", "exp", "log", "abs", "sqrt", "round", "ceil", "floor"];
+    if (parsedStringInputArray[0] != "MTools") {
+      return { isValid: false, errorMessage: `only MTools package supported by this app. input must start with 'MTools'` };
+    }
+    if (parsedStringInputArray.length != 3) {
+      return { isValid: false, errorMessage: `command line input must contain exactly 3 elements: 'packageName commandName arguments'` };
+    }
+    if (validCommandList.indexOf(parsedStringInputArray[1]) == -1) {
+      return { isValid: false, errorMessage: `MTools only supports the following commands: ${validCommandList.join(",")}` };
+    }
+    if (!MTools.allStringElementsOfArrayContainNumbers(parsedStringInputArray[2].split(","))) {
+      return { isValid: false, errorMessage: `last element of command line input, arguments, should contain only numbers and commas` };
+    }
+
+    return { isValid: true, errorMessage: "" };
+  }
+
+  static commandArgumentsValidator(commandArgsArray: string[]) {
+    let singleArgumentCommands = ["abs", "sqrt", "ceil", "round", "floor"];
+    let doubleArgumentCommands = ["add", "subtract", "divide", "multiply", "exp", "log"];
+    let argsArray = commandArgsArray[1].split(",").map((stringArg) => Number(stringArg));
+
+    // 与えられたコマンドが単一の引数を必要とする場合、コマンドと引数をsingle argument validatorに渡します。
+    if (singleArgumentCommands.indexOf(commandArgsArray[0]) != -1) {
+      return MTools.singleArgValidator(commandArgsArray[0], argsArray);
+    }
+
+    // 与えられたコマンドが2つの引数を必要とする場合、コマンドと引数をdouble argument validatorに渡します。
+    if (doubleArgumentCommands.indexOf(commandArgsArray[0]) != -1) {
+      return MTools.doubleArgValidator(commandArgsArray[0], argsArray);
+    }
+
+    return { isValid: false, errorMessage: "eee" };
+  }
+
+  static singleArgValidator(commandName: string, argsArray: number[]) {
+    if (argsArray.length != 1) return { isValid: false, errorMessage: `command ${commandName} requires exactly 1 argument` };
+    if (commandName == "sqrt" && argsArray[1] < 0) return { isValid: false, errorMessage: `command ${commandName} only supports arguments with value >= 0` };
+
+    return { isValid: true, errorMessage: "" };
+  }
+
+  static doubleArgValidator(commandName: string, argsArray: number[]) {
+    if (argsArray.length != 2) {
+      return { isValid: false, errorMessage: `command ${commandName} requires exactly 2 arguments` };
+    }
+    if (commandName == "divide" && argsArray[1] == 0) {
+      return { isValid: false, errorMessage: `command ${commandName} requires divisors != 0` };
+    }
+    if ((commandName == "log" && argsArray[0] <= 0) || argsArray[0] == 1) {
+      return { isValid: false, errorMessage: `command ${commandName} requires a base > 0 and not equal to 1` };
+    }
+    if ((commandName == "log" && argsArray[0] <= 0) || argsArray[0] == 1) {
+      return { isValid: false, errorMessage: `command ${commandName} requires a positive antilogarithm` };
+    }
+
+    return { isValid: true, errorMessage: "" };
+  }
+
+  static allStringElementsOfArrayContainNumbers(inputArray: string[]) {
+    return inputArray.reduce((elementsAreNumbers, currentElement) => {
+      let parsedNum = Number(currentElement);
+      return elementsAreNumbers && typeof parsedNum == "number" && !isNaN(parsedNum);
+    }, true);
+  }
+
+  static evaluatedResultsStringFromParsedCLIArray(PCA: string[]) {
+    let result = 0;
+    let argsArray = PCA[2].split(",").map((stringArgument) => Number(stringArgument));
+    let argA = argsArray[0];
+    let argB = argsArray[1];
+
+    if (PCA[1] == "add") result = argA + argB;
+    else if (PCA[1] == "subtract") result = argA - argB;
+    else if (PCA[1] == "multiply") result = argA * argB;
+    else if (PCA[1] == "divide") result = argA / argB;
+    else if (PCA[1] == "exp") result = Math.pow(argA, argB);
+    else if (PCA[1] == "log") result = Math.log(argB) / Math.log(argA);
+    else if (PCA[1] == "sqrt") result = Math.sqrt(argA);
+    else if (PCA[1] == "abs") result = Math.abs(argA);
+    else if (PCA[1] == "round") result = Math.round(argA);
+    else if (PCA[1] == "ceil") result = Math.ceil(argA);
+    else if (PCA[1] == "floor") result = Math.floor(argA);
+    else console.log("MTools.evaluatedResultsStringFromParsedCLIArray:: invalid command name");
+
+    return "your result is: " + result;
+  }
+}
+
 class View {
   public static submitSearch(event: KeyboardEvent, user: User, commandList: CommandList) {
     if (event.key === "Enter") {
+      let parsedCLIArray = MTools.commandLineParser(config.CLITextInput.value);
       let header = Controller.getCommandHeader(user);
       Controller.addCommandToList(commandList);
       Controller.appendCommandToCLIOutputDiv(header);
+      let validatorResponse = MTools.parsedArrayValidator(parsedCLIArray);
+      if (validatorResponse["isValid"] == false) MTools.appendResultParagraph(config.CLIOutputDiv, false, validatorResponse["errorMessage"]);
+      else MTools.appendResultParagraph(config.CLIOutputDiv, true, MTools.evaluatedResultsStringFromParsedCLIArray(parsedCLIArray));
     } else if (event.key === "ArrowUp") {
       Controller.showPrevCommand(commandList);
     } else if (event.key === "ArrowDown") {
